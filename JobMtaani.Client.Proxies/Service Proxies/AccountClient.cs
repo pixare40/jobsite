917849885cd33b29e8600832cfc06d648b1caae6
Kabaji Egara <kabajiegara@live.com>
2016-05-24 22:41:26 +0100

    Added Account Client

---
+++ JobMtaani.Client.Proxies/Service Proxies/AccountClient.cs
@@ -0,0 +1,24 @@
+﻿using JobMtaani.Client.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using JobMtaani.Client.Entities;
+
+namespace JobMtaani.Client.Proxies.Service_Proxies
+{
+    public class AccountClient : ClientBase<IAccountService>, IAccountService
+    {
+        public Account GetCustomerAccountInfo(string loginEmail)
+        {
+            return Channel.GetCustomerAccountInfo(loginEmail);
+        }
+
+        public Task<Account> GetCustomerInfoAsync(string loginEmail)
+        {
+            return Channel.GetCustomerInfoAsync(loginEmail);
+        }
+    }
+}
