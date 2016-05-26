94ed761fcc4cc4a7046cfb0b158465fc2e8a7bce
Kabaji Egara <kabajiegara@live.com>
2016-05-26 20:48:15 +0100

    Added MVC app and Web API project

---
+++ JobMtaani.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JobMtaani.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ JobMtaani.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JobMtaani.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
